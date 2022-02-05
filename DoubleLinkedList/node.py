2596ad1a6deb081d194a451153bca742d980fdc4
Anton <91902160+TeleginAnton@users.noreply.github.com>
2022-02-05 12:47:41 +0300

    add node.py

---
+++ DoubleLinkedList/node.py
@@ -0,0 +1,26 @@
+from typing import Any, Optional
+
+
+class Node:
+    """ Класс, который описывает узел связного списка. """
+
+    def __init__(self, value: Any, next_: Optional["Node"] = None):
+        self.value = value
+
+        self.next = None
+        self.set_next(next_)
+
+    def set_next(self, next_: Optional["Node"] = None) -> None:
+        self.is_valid(next_)
+        self.next = next_
+
+    def __repr__(self) -> str:
+        return f"Node({self.value}, {None})" if self.next is None else f"Node({self.value}, Node({self.next}))"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    @staticmethod
+    def is_valid(node: Any) -> None:
+        if not isinstance(node, (type(None), Node)):
+            raise TypeError
