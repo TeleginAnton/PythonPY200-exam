a8181106fbcfcc4ee78b1339bec198d7158256bf
Anton <91902160+TeleginAnton@users.noreply.github.com>
2022-02-06 16:03:57 +0300

    fix node.py

--- DoubleLinkedList/node.py
+++ DoubleLinkedList/node.py
@@ -5,14 +5,18 @@ class Node:
     """ Класс, который описывает узел связного списка. """
 
     def __init__(self, value: Any, next_: Optional["Node"] = None):
+
         self.value = value
-
-        self.next = None
-        self.set_next(next_)
-
-    def set_next(self, next_: Optional["Node"] = None) -> None:
-        self.is_valid(next_)
         self.next = next_
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, next_: Optional["Node"]):
+        self.is_valid(next_)
+        self._next = next_
 
     def __repr__(self) -> str:
         return f"Node({self.value}, {None})" if self.next is None else f"Node({self.value}, Node({self.next}))"
@@ -23,4 +27,4 @@ class Node:
     @staticmethod
     def is_valid(node: Any) -> None:
         if not isinstance(node, (type(None), Node)):
-            raise TypeError
+            raise TypeError(f"the expected type is an integer")
